717ce37aba69f469e9e9b5d74747a1cb872a98e8
WMXPY <wm@mengw.io>
2020-08-09 11:07:37 -0500

    add pending config

--- src/pending.ts
+++ src/pending.ts
@@ -6,7 +6,34 @@
 
 import { IResponseConfig } from "./declare";
 
+export type PendingRequestCreateOption<Body extends any = any, Data extends any = any> = {
+
+    readonly response: Promise<IResponseConfig<Data>>;
+    readonly abort: () => void;
+};
+
 export class PendingRequest<Body extends any = any, Data extends any = any> {
 
+    public static create<Body extends any = any, Data extends any = any>(option: PendingRequestCreateOption<Body, Data>): PendingRequest<Body, Data> {
+
+        return new PendingRequest(option);
+    }
+
     private readonly _response: Promise<IResponseConfig<Data>>;
+    private readonly _abort: () => void;
+
+    private constructor(option: PendingRequestCreateOption<Body, Data>) {
+
+        this._response = option.response;
+        this._abort = option.abort;
+    }
+
+    public get response(): Promise<IResponseConfig<Data>> {
+        return this._response;
+    }
+
+    public abort(): void {
+
+        this._abort();
+    }
 }
