56e46ff4cf9f1c6cf11d719ece3670174666b608
WMXPY <wm@mengw.io>
2020-08-09 18:01:16 -0500

    implement abort function

--- test/unit/pending.test.ts
+++ test/unit/pending.test.ts
@@ -5,6 +5,7 @@
  * @override Unit
  */
 
+import { Sleep } from "@sudoo/asynchronous";
 import { expect } from "chai";
 import * as Chance from "chance";
 import { IResponseConfig } from "../../src/declare";
@@ -49,6 +50,8 @@ describe('Given {PendingRequest} Class', (): void => {
 
             // eslint-disable-next-line @typescript-eslint/require-await
             response: (async () => {
+                // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+                await Sleep.for(30)
                 return createMockResponseConfig(data, chance);
             })(),
             abort: () => {
@@ -56,11 +59,24 @@ describe('Given {PendingRequest} Class', (): void => {
             },
         });
 
-        const response: IResponseConfig<string> = await pending.response;
+        setTimeout(() => {
+            pending.abort();
+            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        }, 10);
 
-        expect(response.data).to.be.equal(data);
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        expect(aborted).to.be.false;
+        let error: any;
+
+        try {
+            const response: IResponseConfig<string> = await pending.response;
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(response).to.be.undefined;
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(aborted).to.be.true;
+        } catch (reason) {
+            error = reason;
+        } finally {
+            expect(error.message).to.be.equal("[Barktler] Aborted");
+        }
     });
 
     it('should be able to double return response', async (): Promise<void> => {
