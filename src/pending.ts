4b918889d23aba99408a0f12e4fbfb8db3e6059b
WMXPY <wm@mengw.io>
2020-11-13 14:22:47 -0600

    remove on error hook

--- src/pending.ts
+++ src/pending.ts
@@ -6,8 +6,6 @@
 
 import { IResponseConfig } from "./declare";
 
-export type PendingRequestErrorHandlingFunction = (reason: any) => void;
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type PendingRequestCreateOption<Body extends any = any, Data extends any = any> = {
 
@@ -31,8 +29,6 @@ export class PendingRequest<Body extends any = any, Data extends any = any> {
     private readonly _response: Promise<IResponseConfig<Data>>;
     private readonly _abort: () => void;
 
-    private _onError: PendingRequestErrorHandlingFunction | null;
-
     private _abortResponse?: () => void;
 
     private constructor(
@@ -43,7 +39,6 @@ export class PendingRequest<Body extends any = any, Data extends any = any> {
         this._succeed = false;
 
         this._abort = option.abort;
-        this._onError = null;
 
         this._response = new Promise<IResponseConfig<Data>>((
             resolve: (data: IResponseConfig<Data>) => void,
@@ -60,10 +55,12 @@ export class PendingRequest<Body extends any = any, Data extends any = any> {
 
                 this._pending = false;
                 this._succeed = true;
+
                 resolve(value);
             }).catch((reason: any) => {
 
                 this._pending = false;
+
                 reject(reason);
             });
         });
@@ -85,25 +82,11 @@ export class PendingRequest<Body extends any = any, Data extends any = any> {
         return this._response;
     }
 
-    public setErrorHook(method: PendingRequestErrorHandlingFunction): this {
-
-        this._onError = method;
-        return this;
-    }
-
     public abort(): void {
 
         this._abort();
         if (this._abortResponse) {
             this._abortResponse();
         }
-    }
-
-    private _triggerErrorHook(reason: any): this {
-
-        if (typeof this._onError === 'function') {
-            this._onError(reason);
-        }
-        return this;
     }
 }
