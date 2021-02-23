eecf0cde63b7902ff51d8cb1b9c4f66dd9b9f584
WMXPY <wm@mengw.io>
2021-02-23 11:15:29 -0600

    fix mock driver

--- test/mock/mock.ts
+++ test/mock/mock.ts
@@ -10,6 +10,9 @@ import { IResponseConfig } from "../../src/declare";
 export const createMockResponseConfig = (data: string, chance: Chance.Chance): IResponseConfig<string> => {
 
     return {
+
+        succeed: true,
+
         data,
         status: 200,
         statusText: chance.string(),
